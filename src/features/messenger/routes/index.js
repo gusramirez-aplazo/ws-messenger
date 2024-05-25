f2401e8f881854ecb3e0aa859cf10b509f08bdc0
Gus Ramírez <gustavo@aplazo.mx>
2024-05-24 22:48:11 -0600

    feat: party endpoint

--- src/features/messenger/routes/index.js
+++ src/features/messenger/routes/index.js
@@ -122,4 +122,102 @@ messengerRouter.post('/misa', phoneValidator, async (req, res) => {
   }
 });
 
+messengerRouter.post('/party', phoneValidator, async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ ok: false, errors: errors.array() });
+  }
+
+  const { phone } = req.body;
+
+  try {
+    await wsClient.getState().catch((err) => {
+      throw new RuntimeMsgrError(
+        'Error al obtener el estado del cliente::' + err,
+        'WsClient::getState::unknown'
+      );
+    });
+
+    const signedUrl = await getSignedUrl(
+      s3Client,
+      new GetObjectCommand({ Bucket: 'my-media', Key: 'kids.webp' }),
+      { expiresIn: 60 * 2 }
+    ).catch((err) => {
+      throw new RuntimeMsgrError(
+        'Error al obtener la URL firmada::' + err,
+        'GetSignedUrl::unknown'
+      );
+    });
+
+    const media = await wsPckg.MessageMedia.fromUrl(signedUrl).catch((err) => {
+      throw new RuntimeMsgrError(
+        'Error al obtener message media::' + err,
+        'MessageMedia::fromUrl::unknown'
+      );
+    });
+
+    const numberId = await wsClient.getNumberId(phone).catch((err) => {
+      throw new RuntimeMsgrError(
+        'Error al obtener el número de teléfono::' + err,
+        'WsClient::getNumberId::unknown'
+      );
+    });
+
+    if (!numberId) {
+      return res.status(404).json({
+        ok: false,
+        error: 'El número de teléfono no se encuentra en Whatsapp',
+      });
+    }
+
+    const isRegistered = await wsClient
+      .isRegisteredUser(numberId._serialized)
+      .catch((err) => {
+        throw new RuntimeMsgrError(
+          'Error al verificar si el número de teléfono está registrado::' + err,
+          'WsClient::isRegisteredUser::unknown'
+        );
+      });
+
+    if (!isRegistered) {
+      return res.status(404).json({
+        ok: false,
+        error:
+          'Parece que el número de teléfono no está registrado como usuario de Whatsapp',
+      });
+    }
+
+    const result = await wsClient
+      .sendMessage(numberId?._serialized, media, {
+        caption: misaMapsUrl,
+      })
+      .catch((err) => {
+        throw new RuntimeMsgrError(
+          'Error al enviar el mensaje::' + err,
+          'WsClient::sendMessage::unknown'
+        );
+      });
+
+    return res.json({
+      result: result.id.id,
+      ok: true,
+      error: null,
+    });
+  } catch (error) {
+    if (error instanceof RuntimeMsgrError) {
+      return res.status(500).json({
+        ok: false,
+        error: error.message,
+        code: error.code,
+      });
+    }
+
+    return res.status(500).json({
+      ok: false,
+      error: error?.message ?? error?.error ?? error,
+    });
+  }
+});
+
 export { messengerRouter };
